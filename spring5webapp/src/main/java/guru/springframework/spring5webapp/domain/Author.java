b2597bbce6b77280d69511e333111b1a2d60535b
Brad Julian <brad.julian@resonate.com>
2020-03-20 13:51:36 -0400

    Complete "Equality in Hibernate", section 2.15

--- spring5webapp/src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ spring5webapp/src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -1,6 +1,7 @@
 package guru.springframework.spring5webapp.domain;
 
 import javax.persistence.*;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -57,5 +58,26 @@ public class Author {
     this.books = books;
   }
 
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Author author = (Author) o;
+    return Objects.equals(id, author.id);
+  }
 
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
+
+  @Override
+  public String toString() {
+    return "Author{" +
+        "id=" + id +
+        ", firstName='" + firstName + '\'' +
+        ", lastName='" + lastName + '\'' +
+        ", books=" + books +
+        '}';
+  }
 }
