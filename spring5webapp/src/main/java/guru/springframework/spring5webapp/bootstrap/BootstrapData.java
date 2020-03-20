f0e3c3857f9301f39ebf96a4b57735a6a183bbdf
Brad Julian <brad.julian@resonate.com>
2020-03-20 14:15:06 -0400

    Complete "Add Publisher Entity", section 2 assignment 2

--- spring5webapp/src/main/java/guru/springframework/spring5webapp/bootstrap/BootstrapData.java
+++ spring5webapp/src/main/java/guru/springframework/spring5webapp/bootstrap/BootstrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.domain.Author;
 import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,14 +14,18 @@ public class BootstrapData implements CommandLineRunner {
 
   private final AuthorRepository authorRepository;
   private final BookRepository bookRepository;
+  private final PublisherRepository publisherRepository;
 
-  public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+  public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
     this.authorRepository = authorRepository;
     this.bookRepository = bookRepository;
+    this.publisherRepository = publisherRepository;
   }
 
   @Override
   public void run(String... args) throws Exception {
+
+    System.out.println("Started in Bootstrap");
 
     Author eric = new Author("Eric", "Evans");
     Book ddd = new Book("Domain Driven Design", "123123");
@@ -38,7 +44,12 @@ public class BootstrapData implements CommandLineRunner {
     authorRepository.save(rod);
     bookRepository.save(noEJB);
 
-    System.out.println("Started in Bootstrap");
     System.out.println("Number of Books: " + bookRepository.count());
+
+    Publisher randomHouse = new Publisher("Random House", "123 Book Lane", "Binding", "PA", "83240");
+
+    publisherRepository.save(randomHouse);
+
+    System.out.println("Number of Publishers: " + publisherRepository.count());
   }
 }
