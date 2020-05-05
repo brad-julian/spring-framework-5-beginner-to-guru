d586b087899fcfa73494b86bca742d82e6823d55
Brad Julian <brad.julian@resonate.com>
2020-05-05 16:56:51 -0400

    Complete "Multi-Module Builds", section 3.48

--- sfg-pet-clinic/pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/web/PetClinicWebApplicationTests.java
+++ sfg-pet-clinic/pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/web/PetClinicWebApplicationTests.java
@@ -1,13 +1,13 @@
-package guru.springframework.sfgpetclinic;
+package guru.springframework.sfgpetclinic.web;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SfgPetClinicApplicationTests {
+class PetClinicWebApplicationTests {
 
-  @Test
-  void contextLoads() {
-  }
+	@Test
+	void contextLoads() {
+	}
 
 }
